8929df01a3cafce6809626149664464a0f163f7e
Will Wilson <williamrwilson@gmail.com>
2017-04-09 15:59:35 -0400

    basic index view working

--- express-app.js
+++ express-app.js
@@ -2,35 +2,43 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 const MongoClient = require('mongodb').MongoClient;
-//MongoClient.connect('link-to-mongodb', (err, database) => {
-  db.connect({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS
-  })
-//})
+var db;
+app.set('view-engine', 'ejs');
+
+MongoClient.connect("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASS + process.env.DB_URL, (err, database) => {
+    if (err) return console.log(err);
+    db = database
+    app.listen(3000, () => {
+        console.log("listening!");
+    })
+})
 
 var bodyParser = require('body-parser');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/index.html', function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  db.collection('transactions').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    var sum = sumTransactions(result);
+
+    res.render('index.ejs', {trans: result, sum: sum });
+  })
 })
+
+var sumTransactions = function(arr) {
+  var sum = 0;
+  for (var i = 0; i<arr.length; i++) {
+    sum += Number(arr[i].amount);
+  }
+  return sum;
+}
 
 app.post('/new_transaction', urlencodedParser, function(req, res){
-  response = {
-    transDate: req.body.date,
-    transAmount: req.body.amount,
-    note: req.body.notes,
-    category: req.body.category
-  };
-  console.log(response);
-  res.end(JSON.stringify(response));
-})
 
-var server = app.listen(3000, function(){
-  var host = server.address().address
-  var port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port)
+  db.collection('transactions').save(req.body, (err, result) =>{
+    if (err) return console.log(err)
+    console.log('saved!')
+    res.redirect('/index.html')
+  })
 })
